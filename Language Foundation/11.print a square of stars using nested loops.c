8e14570c74f71108619bd1598da7466e2f801bc0
Hornet160 <91388010+Hornet160@users.noreply.github.com>
2023-05-24 01:55:28 +0600

    Create 11.print a square of stars using nested loops.c
    Create a program that displays on the screen a square of n x n stars, with the integer n given as an input.
    
    Examples
    Input:
    5
    Output:
    *****
    *****
    *****
    *****
    *****

---
+++ Language Foundation/11.print a square of stars using nested loops.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void){
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        // printf("\n");
+        for(j=0;j<5;j++){
+            printf("*");
+        
+        }
+         printf("\n");
+    }
+    return 0;
+}
