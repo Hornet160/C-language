acae1275559830bfd27e163f019c2a577660b495
Hornet160 <91388010+Hornet160@users.noreply.github.com>
2023-05-24 01:21:57 +0600

    6.performing simple arithmetic.c

---
+++ Getting Started/6.performing simple arithmetic.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+    printf("Dear Procrastinator,\nyou still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!", 2, (60*24*2), (60*(60*24*2)));
+    return 0;
+}
