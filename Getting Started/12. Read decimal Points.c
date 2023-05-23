b1972b975db4e1d5c77c97066154716dc66864ea
Hornet160 <91388010+Hornet160@users.noreply.github.com>
2023-05-24 01:35:17 +0600

    Create 12. Read decimal Points.c
    Petra, Rémi and their families went hiking in the mountains together and realized that distances are measured in different units in France and the United States. To help them convert between units, please write a program that reads a decimal number representing a distance in kilometers and that prints out the corresponding distance in miles with 6 decimal places. 
    
    You may use the fact that one kilometer equals 0.621371 miles. 
    
    Example
    Input:
    4.8
    Output: 
    2.982581

---
+++ Getting Started/12. Read decimal Points.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    double distance ;
+    double milesToKm=0.621371 ;
+    //double distance2=distance/4.8;
+    scanf("%lf",&distance);
+    printf("%lf",distance*milesToKm);
+
+    return 0;
+
+}
