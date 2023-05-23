de8cf28a753d610a649fab86974c2ef55789ae94
Hornet160 <91388010+Hornet160@users.noreply.github.com>
2023-05-24 01:36:08 +0600

    Create 13.Divide Numbers.c
    When Rémi came to the US (to visit Petra to make this MOOC) he brought his favorite cookie recipe! When trying to bake the cookies he realized that ovens in the US show temperature in degrees Fahrenheit, but the cookie recipe called for a temperature in degrees Celsius. We need your help!
    
    Please write a C-program that reads a decimal number representing a temperature in degrees Celsius and prints out the corresponding temperature in degrees Fahrenheit with 1 decimal place. Here is the conversion formula:
    
    Temperature (°F) = Temperature (°C) × 9.0 / 5.0 + 32.0
    
    Examples
    Input:
    192
    Output:
    377.6

---
+++ Getting Started/13.Divide Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+   double Temperature1;
+   scanf("%lf",&Temperature1);
+   double Temperature;
+   Temperature=Temperature1*9.0/5.0+32.0;
+
+
+   printf("%.1lf",Temperature);
+
+    return 0;
+
+}
