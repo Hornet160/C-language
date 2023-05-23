1ecebc8ae70b62a64f5d4e8bc818091c392120ed
Hornet160 <91388010+Hornet160@users.noreply.github.com>
2023-05-24 01:37:12 +0600

    Create 14.Find remainder of Integer Division.c
    You have a number of loose matches that you would like to put back into boxes. Write a program that calculates and displays how many full boxes you will have and how many leftover matches you will have after filling all the boxes you can. Your program should take as input the number of matches to be boxed up followed by the size of a each box. Next it should print out the number of full boxes followed by the number of remaining matches.
    
    Example
    Input:
    666
    13
    Output: 
    51
    3

---
+++ Getting Started/14.Find remainder of Integer Division.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void){
+    //fill the match box
+    int matches;
+    int rest;
+    scanf("%d",&matches);
+    scanf("%d",&rest);
+    printf("%d\n",matches/rest );
+
+    printf("%d\n",matches%rest);
+
+
+    return 0;
+
+}
+
